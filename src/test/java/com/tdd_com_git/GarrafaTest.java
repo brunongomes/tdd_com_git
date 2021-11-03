d083d87b39a6f925e0fe581d5905376fa0475dbe
Vinícius Duarte Lopes <vin.duartelopes@gmail.com>
2021-11-03 20:11:52 -0300

    Formata código de GarrafaTest

--- src/test/java/com/tdd_com_git/GarrafaTest.java
+++ src/test/java/com/tdd_com_git/GarrafaTest.java
@@ -5,25 +5,25 @@ import org.junit.Test;
 
 public class GarrafaTest {
 
-    private Garrafa garrafa;
+   private Garrafa garrafa;
 
-    @Test
-    public void capacidadeEVolumeInicialTest() {
-    int capacidade = 1000;
-    int volume = 700;
-    garrafa = new Garrafa(capacidade, volume);
+   @Test
+   public void capacidadeEVolumeInicialTest() {
+      int capacidade = 1000;
+      int volume = 700;
+      garrafa = new Garrafa(capacidade, volume);
 
-    assertEquals(capacidade, garrafa.getCapacidade());
-    assertEquals(volume, garrafa.getVolume());
-    }
+      assertEquals(capacidade, garrafa.getCapacidade());
+      assertEquals(volume, garrafa.getVolume());
+   }
 
-    @Test
-    public void capacidadeEVolumeInicial2Test() {
-    int capacidade = 900;
-    int volume = 200;
-    garrafa = new Garrafa(capacidade, volume);
+   @Test
+   public void capacidadeEVolumeInicial2Test() {
+      int capacidade = 900;
+      int volume = 200;
+      garrafa = new Garrafa(capacidade, volume);
 
-    assertEquals(capacidade, garrafa.getCapacidade());
-    assertEquals(volume, garrafa.getVolume());
-    }
+      assertEquals(capacidade, garrafa.getCapacidade());
+      assertEquals(volume, garrafa.getVolume());
+   }
 }
